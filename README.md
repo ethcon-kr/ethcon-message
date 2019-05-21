392ada7e3eb05d00f399d07d5f5c5559a3104bf3
Jin <jin.s@onther.io>
2019-05-21 23:04:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Simple Message Store for Ethconkr 2019
+
+It is going to use for ethcon-board project.
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var ethboard = artifacts.require("DSMessage");
+
+module.exports = function(deployer) {
+  deployer.deploy(ethboard);
+};
